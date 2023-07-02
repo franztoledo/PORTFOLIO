6c95377642e97f170485e92833db87b6999e3239
Franz Toledo <franz.wilber.tm@gmail.com>
2023-07-02 11:24:22 -0500

    logo y estilos

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { FaBars, FaTimes } from "react-icons/fa";
-import { Link } from "react-scroll";
+
 function Navbar() {
   return (
     <div className='flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed'>
-      <FaBars/>
+      <div>
+        <h1 className='text-4xl font-signature ml-2 tracking-widest leading-3'>My Portfolio</h1>
+      </div>
     </div>
   )
 }
