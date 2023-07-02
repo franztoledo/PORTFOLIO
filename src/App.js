b8d80f5ec09a95e41efd5e132fd13cc6e2ff7a5d
Franz Toledo <franz.wilber.tm@gmail.com>
2023-07-02 15:51:07 -0500

    sociallinks in app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Home } from "./components/Home";
 import { Navbar } from "./components/Navbar";
+import { SocialLinks } from "./components/SocialLinks";
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Home/>
+      <SocialLinks/>
     </div>
   );
 }
