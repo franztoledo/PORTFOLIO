cd1f572c5e72f85779bdf3df3d21b151c4699d96
Franz Toledo <franz.wilber.tm@gmail.com>
2023-07-02 10:33:34 -0500

    icon y estilos principales para el navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
-
+import { FaBars, FaTimes } from "react-icons/fa";
+import { Link } from "react-scroll";
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className='flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed'>
+      <FaBars/>
+    </div>
   )
 }
 
