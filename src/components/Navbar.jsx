3dc3eee2bf34a0c839170de3411c89dd27cdc4fa
Franz Toledo <franz.wilber.tm@gmail.com>
2023-07-02 12:04:12 -0500

    nav options mobile styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -47,7 +47,12 @@ function Navbar() {
       >
         {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
       </div>
-
+      <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500">
+        <li className="px-4 cursor-pointer capitalize py-6 text-4xl">home</li>
+        <li className="px-4 cursor-pointer capitalize py-6 text-4xl">home</li>
+        <li className="px-4 cursor-pointer capitalize py-6 text-4xl">home</li>
+      
+      </ul>
     </div>
   )
 }
