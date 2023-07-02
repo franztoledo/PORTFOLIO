30daadf0f3c1e71549ef10c4792e26ed1aaa6c13
Franz Toledo <franz.wilber.tm@gmail.com>
2023-07-02 15:44:55 -0500

    principal home final

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import HeroImage from '../assets/heroimage.jpg'
+import HeroImage from '../assets/heroimage2.jpg'
 import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 function Home() {
   return (
@@ -7,19 +7,31 @@ function Home() {
       name="home"
       className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800"
     >
-      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
-        <div className="flex flex-col justify-center h-full">
-          <h2 className="text-4xl sm:text-7xl font-bold text-white">I'm a Fronted Developer</h2>
-          <p className="text-gray-500 py-4 max-w-md">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus at aut ea facilis laborum corrupti nihil iure, sit incidunt provident quaerat minus nesciunt neque eius ab beatae. Odio, amet cum.</p>
+      
+      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-20 md:flex-row">
+        <div className="flex flex-col justify-center align-middle h-full gap-8 text-center">
           <div>
-            <button>
+            <img
+              src={HeroImage}
+              alt="my profile"
+              className=" mx-auto w-80 mt-9 rounded-full object-contain "
+            />
+          </div>
+          <h2 className="text-4xl md:text-7xl font-bold text-white text-center">I'm a Fronted Developer</h2>
+          <p className="text-gray-500 py-4">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus at aut ea facilis laborum corrupti nihil iure, sit incidunt provident quaerat minus nesciunt neque eius ab beatae. Odio, amet cum.</p>
+          <div>
+            <button
+              duration={500}
+              className="group text-white w-fit px-6 py-3 my-2 mx-auto flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer"
+            >
               Portfolio
-              <span>
-                <MdOutlineKeyboardArrowRight/>
+              <span className="group-hover:rotate-90 duration-300">
+                <MdOutlineKeyboardArrowRight size={25} className="ml-1" />
               </span>
             </button>
           </div>
         </div>
+        
       </div>
     </div>
   )
