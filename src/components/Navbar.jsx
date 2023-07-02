d5e19ab37bf21bf6cec26c2ee483129c971be582
Franz Toledo <franz.wilber.tm@gmail.com>
2023-07-02 11:44:27 -0500

    navbutton function

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -41,8 +41,11 @@ function Navbar() {
           </li>
         ))}
       </ul>
-      <div className="cursor-pointer pr-4 z-10 text-gray-500 md:hidden">
-        <FaBars size={30}/>
+      <div 
+        onClick={() => setNav(!nav)} 
+        className="cursor-pointer pr-4 z-10 text-gray-500 md:hidden"
+      >
+        {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
       </div>
 
     </div>
