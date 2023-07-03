64014416595a639aeda4aae7e3a5ea94f7cf5681
Franz Toledo <franz.wilber.tm@gmail.com>
2023-07-03 00:37:25 -0500

    portfolio map and styles

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -46,15 +46,17 @@ function Portfolio() {
       id="projects"
       className=" h-screen bg-gradient-to-b from-black via-black to-gray-800 w-full px-5 md:px-20 text-white"
     >
-      <div className="flex  items-center text-center">
+      <div className="flex  items-center text-center p-4">
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 border-gray-500">
-            About
+            Projects
           </p>
         </div>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 xl:gap-14">
-        
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 xl:gap-14 p-4">
+        {projects.map(({ id, title, des, srcgit,srcweb})=>(
+          <div>{title}</div>
+        ))}
       </div>
     </section>
   );
