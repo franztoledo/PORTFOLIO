18b119be70ccf050cbe58bd03ee963a7995f406c
Franz Toledo <franz.wilber.tm@gmail.com>
2023-07-03 05:58:01 -0500

    width portfolio part and bg fix experience

--- src/components/Experiencie.jsx
+++ src/components/Experiencie.jsx
@@ -55,7 +55,7 @@ function Experiencie() {
   return (
     <div
       name="experience"
-      className="bg-gradient-to-b px-5 md:px-20 from-black via-black to-gray-800 w-full h-screen"
+      className=" px-5 md:px-20 bg-gradient-to-b from-gray-800 to-black w-full h-screen"
     >
       <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white">
         <div>
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -55,16 +55,16 @@ function Portfolio() {
   return (
     <section
       id="projects"
-      className=" bg-gradient-to-b from-black via-black to-gray-800 w-full px-5 md:px-20 text-white"
+      className=" bg-gradient-to-b from-black via-black to-gray-800 w-full px-5 md:px-20 text-white mx-auto"
     >
-      <div className="flex  items-center text-center p-4">
+      <div className="flex  items-center text-center p-4 max-w-screen-lg mx-auto">
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 border-gray-500">
             Projects
           </p>
         </div>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 xl:gap-14 p-4 px-1 sm:px-12 ">
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 xl:gap-14 p-4 px-1 sm:px-12  max-w-screen-lg mx-auto">
         {projects.map(({ id, title, des, srcgit,srcweb,img,style})=>(
           <Card key={id} title={title} des={des} git={srcgit} demo={srcweb} img={img} shadow={style}/>
         ))}
