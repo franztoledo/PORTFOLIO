fe3f9ccb1ee5d06dc5fdfb58ab565f0de8381a43
Franz Toledo <franz.wilber.tm@gmail.com>
2023-07-03 06:10:41 -0500

    helper files

---
+++ src/helpers/links.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { BsFillPersonLinesFill } from "react-icons/bs";
+import { FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
+
+
+const links = [
+  {
+    id: 1,
+    child: (
+      <>
+        LinkedIn <FaLinkedin size={30} />
+      </>
+    ),
+    href: "https://www.linkedin.com/in/franz-toledo-181a70280/",
+    style: "rounded-tr-md",
+  },
+  {
+    id: 2,
+    child: (
+      <>
+        GitHub <FaGithub size={30} />
+      </>
+    ),
+    href: "https://github.com/franztoledo",
+  },
+  {
+    id: 3,
+    child: (
+      <>
+        Instagram <FaInstagram size={30} />
+      </>
+    ),
+    href: "https://www.instagram.com/franz.toledo.04/",
+    style: "rounded-br-md"
+  },
+  
+];
+
+export {links}
