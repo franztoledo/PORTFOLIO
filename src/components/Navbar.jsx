a741356455ae2ea180f91119fe72c4281c78fa5e
Franz Toledo <franz.wilber.tm@gmail.com>
2023-07-03 06:17:02 -0500

    change name links to linksScroll

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { FaBars, FaTimes } from "react-icons/fa";
 
 function Navbar() {
   const [nav, setNav] = useState(false);
-  const links = [
+  const linksScroll = [
     {
       id: 1,
       link: "home",
@@ -32,7 +32,7 @@ function Navbar() {
       </div>
 
       <ul className='hidden md:flex'>
-        {links.map(({ id, link }) => (
+        {linksScroll.map(({ id, link }) => (
           <li
             key={id}
             className="px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200"
@@ -49,7 +49,7 @@ function Navbar() {
       </div>
       {nav && (
         <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500">
-        {links.map(({ id, link }) => (
+        {linksScroll.map(({ id, link }) => (
           <li
             key={id}
             className="px-4 cursor-pointer capitalize py-6 text-4xl"
--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { links } from '../helpers/links.js';
 
+
 function SocialLinks() {
   
   return (
