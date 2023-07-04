a4fb139514d68a1519c32b41dca52523f94aa28b
Franz Toledo <franz.wilber.tm@gmail.com>
2023-07-04 11:50:19 -0500

    styles for banner y responsive

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,23 +4,37 @@ import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 import { Link } from 'react-scroll';
 import { useTypewriter, Cursor } from 'react-simple-typewriter';
 function Home() {
+  const [text] = useTypewriter({
+    words: ["<Hi, the name is Franz Toledo/>", "I'm a Fronted Developer", ">.Guy Who Loves To Code"],
+    loop: true,
+    
+    delaySpeed: 2000,
+  });
   return (
     <div
       name="home"
       className="pt-20 w-full bg-gradient-to-b from-black via-black to-gray-800"
     >
       
-      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-20 md:flex-row">
-        <div className="flex flex-col justify-center align-middle h-full gap-8 text-center">
+      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-7 md:px-20 md:flex-row">
+        <div className="flex flex-col justify-center align-middle w-full h-full gap-4 text-center xl:gap-8">
           <div>
             <img
               src={HeroImage}
               alt="my profile"
-              className=" mx-auto w-80 mt-9 rounded-full object-contain "
+              className=" mx-auto w-72 sm:w-1/3 rounded-full object-contain "
             />
           </div>
-          <h2 className="text-4xl md:text-7xl font-bold text-white text-center">I'm a Fronted Developer</h2>
-          <p className="text-gray-500 py-4">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus at aut ea facilis laborum corrupti nihil iure, sit incidunt provident quaerat minus nesciunt neque eius ab beatae. Odio, amet cum.</p>
+          <h3 className="text-3xl tracking-widest  text-[#8db0b7] text-center">WELCOME TO MI WOLRD</h3>
+          
+          <h2 className="text-4xl md:text-5xl text-white font-bold pt-4 h-28 md:h-20">
+            <span>{text}</span>
+            <Cursor
+              cursorBlinking="false"
+              cursorStyle="|"
+              cursorColor="#ff014f"
+            />
+          </h2>
           <div>
             <Link
               to="portfolio"
