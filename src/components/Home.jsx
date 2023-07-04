ee62c7f9ccfedc2bad2b634d33cf8d28f45675ae
Franz Toledo <franz.wilber.tm@gmail.com>
2023-07-03 20:04:01 -0500

    Home screen fixed and padding top

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,7 @@ function Home() {
   return (
     <div
       name="home"
-      className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800"
+      className="pt-20 w-full bg-gradient-to-b from-black via-black to-gray-800"
     >
       
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-20 md:flex-row">
